0b345b3d842a4983d5777ff70eb488f2c6f129b5
Vincent Batoufflet <vincent@batoufflet.info>
2019-04-25 05:31:45 +0200

    Minor main command code changes

--- cmd/mkdeb/main.go
+++ cmd/mkdeb/main.go
@@ -10,42 +10,41 @@ import (
 )
 
 var (
-	configDir     string
+	dataDir       string
 	cacheDir      string
 	indexDir      string
 	repositoryDir string
 )
 
 func main() {
-	if err := run(); err != nil {
+	err := run()
+	if err != nil {
 		print.Error("%s", err)
 		os.Exit(1)
 	}
 }
 
 func run() error {
-	// Get directories paths
+	// Set base directories paths
 	dir, err := homedir.Dir()
 	if err != nil {
 		return err
 	}
 
-	configDir = filepath.Join(dir, ".mkdeb")
-	cacheDir = filepath.Join(configDir, "cache")
-	indexDir = filepath.Join(configDir, "recipes/index")
-	repositoryDir = filepath.Join(configDir, "recipes/default")
+	dataDir = filepath.Join(dir, ".mkdeb")
+	cacheDir = filepath.Join(dataDir, "cache")
+	indexDir = filepath.Join(dataDir, "recipes/index")
+	repositoryDir = filepath.Join(dataDir, "recipes/default")
 
 	// Create cache directory if missing
 	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
 		os.MkdirAll(cacheDir, 0755)
 	}
 
-	// Initialize application CLI
+	// Run CLI application
 	app := cli.NewApp()
 	app.Name = "mkdeb"
-	app.Usage = "The Debian packaging helper"
-	app.HideVersion = true
-
+	app.Usage = "Debian packaging helper"
 	app.Commands = []cli.Command{
 		buildCommand,
 		cleanCommand,
@@ -53,6 +52,7 @@ func run() error {
 		updateCommand,
 		versionCommand,
 	}
+	app.HideVersion = true
 
 	return app.Run(os.Args)
 }
